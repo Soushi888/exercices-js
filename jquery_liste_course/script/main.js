3ac67950a1cd1b8216c12ffba22c6fcec5278fd0
Soushi888 <Sacha.pignot@gmail.com>
2020-06-12 00:52:52 -0400

    Update main.js

--- jquery_liste_course/script/main.js
+++ jquery_liste_course/script/main.js
@@ -6,44 +6,65 @@ input.val(""); // Vide le champs input au rechargement de la page
 let ul = $("div ul"); // Liste
 let ul_content = []; // Contenu de la liste (pour vérifier les doublons);
 
-function saisir(valeur_saisie) {
-  $("li").each((index, element) => {
-    // Si un élément est selectionné et que ce qui est saisi dans le champs n'est pas un doublon, change la valeure de L'élément selectionné.
-    if (
-      $(element).hasClass("rectification") &&
-      $.inArray(valeur_saisie.toLowerCase(), ul_content) == -1
-    ) {
-      $(element).text(valeur_saisie);
-      return;
-    }
-  });
-
-  // Vérifie si l'élément saisie n'est pas déjà présent dans la liste.
-  if ($.inArray(valeur_saisie.toLowerCase(), ul_content) == -1) {
-    ul.append("<li>" + valeur_saisie + "</li>");
-    ul_content.push(valeur_saisie.toLowerCase());
+// Selection d'un item de la liste à modifier.
+let rectification = ul.on("click", "li", (evt) => {
+  if ($(event.target).hasClass("rectification")) {
+    $(event.target).removeClass("rectification");
+    return (rectification = false);
   } else {
-    console.log("doublon !");
+    $("li").removeClass("rectification");
+    $(event.target).addClass("rectification");
+    return (rectification = true);
   }
-
-  input.val(""); // Remise à 0 du champs de saisie
-}
+});
 
 input.on("keydown", (evt) => {
   // Si la touche appuyée est Enter et que input contient au moins 1 caractère
   if (evt.key == "Enter" && input.val().trim().length >= 1) {
     let valeur_saisie = input.val().trim();
 
-    saisir(valeur_saisie);
+    // Vérifie si l'item saisie n'est pas déjà présent dans la liste.
+    if (
+      $.inArray(valeur_saisie.toLowerCase(), ul_content) == -1 &&
+      rectification === false
+    ) {
+      ajouterItem(valeur_saisie); // Ajoute l'item
+    } else if (
+      $.inArray(valeur_saisie.toLowerCase(), ul_content) == -1 &&
+      rectification === true
+    ) {
+      modifierItem();
+    }
+    // else {
+    //   console.log("doublon !");
+    // }
+
+    input.val(""); // Remise à 0 du champs de saisie
   }
 });
 
-// Selection d'un élément de la liste à modifier
-ul.on("click", "li", (evt) => {
-  if ($(event.target).hasClass("rectification")) {
-    $(event.target).removeClass("rectification");
-  } else {
-    $("li").removeClass("rectification");
-    $(event.target).addClass("rectification");
-  }
-});
+/**
+ * Ajoute un item à la liste.
+ * @param {string} item
+ */
+function ajouterItem(item) {
+  ul.append("<li>" + item + "</li>");
+  ul_content.push(item.toLowerCase());
+}
+
+/**
+ * Si un item est selectionné et que ce qui est saisi dans le champs n'est pas un doublon, change la valeure de L'item selectionné.
+ *
+ * @param {string} ancien_item
+ * @param {string} nouvel_item
+ */
+function modifierItem(ancien_item, nouvel_item) {
+  $("li").each((index, element) => {
+    if (
+      $(element).hasClass("rectification") &&
+      $.inArray(valeur_saisie.toLowerCase(), ul_content) == -1
+    ) {
+      $(element).text(valeur_saisie);
+    }
+  });
+}
