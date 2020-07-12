90b413dee560d5b6546213956c5d45c9625cfd91
Soushi888 <Sacha.pignot@gmail.com>
2020-07-12 19:37:08 -0400

    Update App.js

--- devoir_04/user_board_storage/script/App.js
+++ devoir_04/user_board_storage/script/App.js
@@ -1,30 +1,57 @@
 "use strict";
+let no_tabs, pos_boite_1, pos_boite_2, master, eq;
+/**
+ * Enregistre l'état du tableau dans le localStorage
+ */
+function enregisterEtat() {
+  no_tabs = $("#tabs").tabs("option", "active");
 
+  pos_boite_1 = $("#dialog1").parent().offset();
+  pos_boite_2 = $("#dialog2").parent().offset();
 
-let no_tabs = $("#tabs").tabs( "option", "active" );
+  master = $("#master").slider("option", "value");
+  eq = $("#eq > span").each(function () {
+    $(this).slider("option", "value");
+  });
 
-// $("#tabs").tabs("option", "active") // Indice de l'onglet (tab) activé (0, 1 ou 2)
-let pos_boite_1 = $("#dialog1").parent().offset() // Objet position (left, top par rapport au document) de la première boite de dialogue  (son parent-wrapper en fait)
-let pos_boite_2 = $("#dialog2").parent().offset() // Idem pour la seconde boite de dialogue
-// let master = $("#master").slider("option", "value"); // Valeur du curseur (slider) horizontal
-// let eq = $("#eq > span").each(function () {
-//     $(this).slider("option", "value") // Valeur de chacun des curseurs verticaux, traitées en boucle jQuery
-// });
+  localStorage.setItem(
+    "user_board_storage",
+    JSON.stringify({
+      tabs: {
+        active: no_tabs,
+      },
+      dialog: {
+        pos1: pos_boite_1,
+        pos2: pos_boite_2,
+      },
+      sliders: {
+        master: master,
+        eq: eq,
+      },
+    })
+  );
 
-localStorage.setItem("user_board_storage", JSON.stringify({
-  tabs: {
-    active: no_tabs
-  },
-  dialog: {
-    pos1: pos_boite_1,
-    pos2: pos_boite_2
-  },
-  // sliders: {
-  //   master: master,
-  //   eq: eq
-  // }
-}));
+  let etat = localStorage.getItem("user_board_storage");
+  console.log(JSON.parse(etat));
+}
 
-let etat = localStorage.getItem('user_board_storage')
+/**
+ * Lit l'état du tableau dans le localStorage et le restitue
+ */
+function lireEtat() {
+  let etat = JSON.parse(localStorage.getItem("user_board_storage"));
+  console.log(etat);
+  $("#tabs").tabs("option", "active", etat.tabs.active);
+  $("#dialog1").parent().offset(etat.dialog.pos1);
+  $("#dialog2").parent().offset(etat.dialog.pos2);
+  $("#master").slider("option", "value", etat.sliders.master);
+  $("#eq > span").each((val, index) => {
+    $("#eq > span").eq(index).slider("option", "value", etat.sliders.eq.index);
+  });
+}
 
-console.log(JSON.parse(etat));
+// Enregitrement de l'état du tableau dans le localStorage lorsque l'utilisateur quitte la page
+window.addEventListener("unload", enregisterEtat);
+
+// Restitue l'état du tableau au chargement de la page
+window.addEventListener("DOMContentLoaded", lireEtat);
