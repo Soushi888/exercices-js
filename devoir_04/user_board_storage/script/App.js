1776e938b0732fe9e95e5f0dc2e83a82cd73568e
Soushi888 <Sacha.pignot@gmail.com>
2020-07-17 21:10:11 -0400

    update

--- devoir_04/user_board_storage/script/App.js
+++ devoir_04/user_board_storage/script/App.js
@@ -1,5 +1,84 @@
 "use strict";
-let tab, pos_boite_1, pos_boite_2, master, eq;
+
+/**
+ * Returns the week number for this date.  dowOffset is the day of week the week
+ * "starts" on for your locale - it can be from 0 to 6. If dowOffset is 1 (Monday),
+ * the week returned is the ISO 8601 week number.
+ * @param int dowOffset
+ * @return int
+ */
+Date.prototype.getWeek = function (dowOffset) {
+  /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+
+  dowOffset = typeof dowOffset == "int" ? dowOffset : 0; //default dowOffset to zero
+  let newYear = new Date(this.getFullYear(), 0, 1);
+  let day = newYear.getDay() - dowOffset; //the day of week the year begins on
+  day = day >= 0 ? day : day + 7;
+  let daynum =
+    Math.floor(
+      (this.getTime() -
+        newYear.getTime() -
+        (this.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
+        86400000
+    ) + 1;
+  let weeknum;
+  //if the year starts before the middle of a week
+  if (day < 4) {
+    weeknum = Math.floor((daynum + day - 1) / 7) + 1;
+    if (weeknum > 52) {
+      let nYear = new Date(this.getFullYear() + 1, 0, 1);
+      let nday = nYear.getDay() - dowOffset;
+      nday = nday >= 0 ? nday : nday + 7;
+      /*if the next year starts before the middle of
+                the week, it is week #1 of that year*/
+      weeknum = nday < 4 ? 1 : 53;
+    }
+  } else {
+    weeknum = Math.floor((daynum + day - 1) / 7);
+  }
+  return weeknum;
+};
+
+/**
+ * Si le nombre donné en paramètre est inclusivement compris entre 1 et 9, ajoute un zéro devant
+ * @param {int} number 
+ * @returns {int}
+ */
+function addZero(number) {
+  switch (number) {
+    case 1:
+      number = "01";
+      break;
+    case 2:
+      number = "02";
+      break;
+    case 3:
+      number = "03";
+      break;
+    case 4:
+      number = "04";
+      break;
+    case 5:
+      number = "05";
+      break;
+    case 6:
+      number = "06";
+      break;
+    case 7:
+      number = "07";
+      break;
+    case 8:
+      number = "08";
+      break;
+    case 9:
+      number = "09";
+      break;
+  }
+
+  return number;
+}
+
+let tab, pos_boite_1, pos_boite_2, master, eq, day, month, date;
 /**
  * Enregistre l'état du tableau dans le localStorage
  */
@@ -15,6 +94,8 @@ function enregisterEtat() {
     eq.push($(this).slider("value"));
   });
 
+  date = new Date($("#datepicker").val());
+
   localStorage.setItem(
     "user_board_storage",
     JSON.stringify({
@@ -29,9 +110,15 @@ function enregisterEtat() {
         master: master,
         eq: eq,
       },
+      date: date,
     })
   );
 }
+
+// Change le numéro de la semaine cqui est affiché à chaque fois que le input date change
+$("#datepicker").on("change", (evt) => {
+  $("#semaine").text("semaine : " + new Date(evt.target.value).getWeek());
+});
 
 /**
  * Lit l'état du tableau dans le localStorage et le restitue
@@ -39,15 +126,27 @@ function enregisterEtat() {
 function lireEtat() {
   let etat = JSON.parse(localStorage.getItem("user_board_storage"));
   console.log(etat);
+
   $("#tabs").tabs("option", "active", etat.tabs.active);
+
   $("#dialog1").parent().offset(etat.dialog.pos1);
   $("#dialog2").parent().offset(etat.dialog.pos2);
+
   $("#master").slider("option", "value", etat.sliders.master);
   $("#eq > span").each((val, index) => {
     eq = $("#eq > span")[val];
-    console.log(eq);
     $(eq).slider("value", etat.sliders.eq[val]);
   });
+  
+  date = new Date(etat.date);
+  month = addZero(date.getMonth() + 1);
+  day = addZero(date.getDate() + 1);
+
+  $("#datepicker").attr(
+    "value",
+    `${date.getFullYear()}-${month}-${day}`
+  );
+  $("#semaine").text("semaine : " + new Date(etat.date).getWeek());
 }
 
 // Enregitrement de l'état du tableau dans le localStorage lorsque l'utilisateur quitte la page
