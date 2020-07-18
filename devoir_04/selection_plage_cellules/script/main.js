f62ae4dfb8ccecd1ac28f6bed89b58fb1739fa2d
Soushi888 <Sacha.pignot@gmail.com>
2020-07-17 21:43:08 -0400

    update

--- devoir_04/selection_plage_cellules/script/main.js
+++ devoir_04/selection_plage_cellules/script/main.js
@@ -1,6 +1,42 @@
 "use strict";
 
+/**
+ * Ajuste la plage sélectionnée puis la retourne sous forme de tableau d'éléments
+ * @param {int} x1
+ * @param {int} y1
+ * @param {int} x2
+ * @param {int} y2
+ * @returns {array}
+ */
+function selectionner(x1, y1, x2, y2) {
+  let c;
+  if (x1 > x2) {
+    c = x1;
+    x1 = x2;
+    x2 = c;
+  }
+  if (y1 > y2) {
+    c = y1;
+    y1 = y2;
+    y2 = c;
+  }
+
+  selection = [];
+  for (let i = x1; i <= x2; ++i) {
+    for (let j = y1; j <= y2; ++j) {
+      td.each((index, element) => {
+        if ($(element).data("coord") == `(${i},${j})`) selection.push(element);
+      });
+    }
+  }
+
+  return selection;
+}
+
 let tr = $("tr");
+let td = $("td");
+let end_point, start_point, selection, x1, y1, x2, y2;
+
 /**
  * Pour chaque td du tableau,
  * on lui associe une coordonnée.
@@ -18,52 +54,46 @@ tr.each((index, element) => {
     });
 });
 
-let td = $("td");
 /**
  * Quand on clique sur une case du tableau,
  * on enregistre sa coordonné
  */
 td.on("mousedown", (evt) => {
   // Réinitialisation de la selection
-  let selection = [];
-  let x1 = null;
-  let y1 = null;
-  let x2 = null;
-  let y2 = null;
+  selection = [];
+  x1 = null;
+  y1 = null;
   td.each((index, element) => {
     $(element).removeClass("selected");
   });
 
-  let start_point = $(evt.target).data("coord");
+  start_point = $(evt.target).data("coord");
 
   x1 = $(evt.target).data("x");
   y1 = $(evt.target).data("y");
-  /**
-   * Quand on relache le clique de la souri,
-   * on enregistre la coordonnée de la case où elle été
-   */
-  td.on("mouseup", (evt) => {
-    let end_point = $(evt.target).data("coord");
+});
 
-    x2 = $(evt.target).data("x");
-    y2 = $(evt.target).data("y");
+/**
+ * Quand on relache le clique de la souri,
+ * on enregistre la coordonnée de la case où elle été
+ */
+td.on("mouseup", (evt) => {
+  end_point = $(evt.target).data("coord");
+  x2 = null;
+  y2 = null;
 
-    // On enregistre dans un tableau toutes les cases selectionnées
-    selection = [];
-    for (let i = x1; i <= x2; ++i) {
-      for (let j = y1; j <= y2; ++j) {
-        td.each((index, element) => {
-          if ($(element).data("coord") == `(${i},${j})`)
-            selection.push(element);
-        });
-      }
-    }
+  x2 = $(evt.target).data("x");
+  y2 = $(evt.target).data("y");
 
-    // On ajoute une couleure de fond aux cases selectionnées
-    for (let i = 0; i < selection.length; ++i) {
-      $(selection[i]).addClass("selected");
-    }
+  // On enregistre dans un tableau toutes les cases selectionnées
+  selectionner(x1, y1, x2, y2);
 
-    console.log(`${start_point}, ${end_point}`);
-  });
+  console.log(selection);
+
+  // On ajoute une couleure de fond aux cases selectionnées
+  for (let i = 0; i < selection.length; ++i) {
+    $(selection[i]).addClass("selected");
+  }
+
+  console.log(`${start_point}, ${end_point}`);
 });
